857d3f57f588893e2643974c909ed68009b05ffc
fumigoro <51395778+fumigoro@users.noreply.github.com>
2023-10-29 06:33:35 +0900

    user_nameプロパティとsexプロパティを削除

--- src/app/_hooks/useUser.tsx
+++ src/app/_hooks/useUser.tsx
@@ -4,56 +4,43 @@ import { atom, useAtom } from 'jotai';
 import { tokenAtom } from '../(auth)/tokenAtom';
 import fetchApi from '@/lib/fetch';
 import { useEffect } from 'react';
-import { atomWithStorage } from 'jotai/utils';
 import { useRouter } from 'next/navigation';
 
 const userAtom = atom<User | undefined>(undefined);
 
 type UserGetResponse = {
   id: string;
-  company_id: string | undefined;
-  user_name: string;
-  display_name: string;
-  sex: string;
   age: number;
+  company_id: string | undefined;
+  display_name: string;
   icon_url: string;
 };
-
-const userIdAtom = atomWithStorage<string | undefined>(
-  'user_id',
-  undefined,
-  undefined,
-  { unstable_getOnInit: true },
-);
 
 const useUser = () => {
   const [token, setToken] = useAtom(tokenAtom);
   const [user, setUser] = useAtom(userAtom);
-  const [userId, setUserId] = useAtom(userIdAtom);
 
   const router = useRouter();
 
   const login = async (email: string, password: string) => {
-    const { userId: currentUserId, token } = await _login(email, password);
+    const { token } = await _login(email, password);
     setToken(token);
-    const user = await getUser(token, currentUserId);
+    const user = await getUser(token);
     setUser(user);
-    setUserId(currentUserId);
   };
 
   useEffect(() => {
     if (!token) return;
-    if (!userId) return;
     (async () => {
       // トークンの有効期限が切れていたらトークンを更新
       const newToken = await getNewTokenIfNeed(token);
       if (newToken && newToken !== token) {
         setToken(newToken);
       }
-      const user = await getUser(token, userId);
+      const user = await getUser(token);
       setUser(user);
     })();
-  }, [userId, token, setUser, setToken]);
+  }, [token, setUser, setToken]);
 
   const logout = () => {
     auth.signOut();
@@ -80,21 +67,19 @@ const _login = async (email: string, password: string) => {
   return { token, userId };
 };
 
-const getUser = async (token: string, userId: string) => {
+const getUser = async (token: string) => {
   // ユーザー情報を取得
   const response = await fetchApi<undefined, UserGetResponse>(
     token,
     'GET',
-    `/user/${userId}`,
+    '/user',
   );
   const body = response.data;
   const user: User = {
     id: body.id,
     companyId: body.company_id,
-    userName: body.user_name,
     displayName: body.display_name,
     iconUrl: body.icon_url,
-    sex: body.sex,
     age: body.age,
   };
   return user;
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,10 +1,8 @@
 type User = {
   id: string;
   companyId: string | undefined;
-  userName: string;
   displayName: string;
   iconUrl: string;
-  sex: string;
   age: number;
 };
 
