12fdc761e97af389d96efa3754d4d49d0a019cd1
fumigoro <51395778+fumigoro@users.noreply.github.com>
2023-10-29 06:10:51 +0900

    不要ファイルを削除

--- src/app/auth/tokenAtom.ts
+++
@@ -1,8 +0,0 @@
-import { atomWithStorage } from 'jotai/utils';
-
-export const tokenAtom = atomWithStorage<string | undefined>(
-  'token',
-  undefined,
-  undefined,
-  { unstable_getOnInit: true },
-);
